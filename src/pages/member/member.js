bb879f58c4f0bed7df0a880c6a7cdecd12105db5
wuwenxing <1428560819@qq.com>
2018-07-20 14:38:26 +0800

    route vue

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,35 @@
-import './member_base.css'
-import './member.css'
+import Vue from 'vue'
+import Router from 'vue-router';
+Vue.use(Router)
+
+
+let routes = [{
+    path: '/',
+    components: require('./components/member.vue')
+}, {
+    path: '/address',
+    components: require('./components/address.vue'),
+    children: [{
+        path: '',
+        // components: require('./components/all.vue')
+        redirect: 'all'
+    }, {
+        path: 'all',
+        components: require('./components/all.vue')
+    }, {
+        path: 'form',
+        components: require('./components/form.vue')
+    }]
+}]
+
+
+//创建router实例
+let router = new Router({
+    routes
+})
+
+//根组件注入
+new Vue({
+    el: '#app',
+    router,
+})
